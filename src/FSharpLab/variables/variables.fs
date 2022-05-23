42642953b26af6213db7d77779ba5f6fb6f7332b
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-05-24 00:13:46 +0300

    Initial commit

---
+++ src/FSharpLab/variables/variables.fs
@@ -0,0 +1,15 @@
+// The "let" keyword defines an (immutable) value
+let myInt = 23
+let myString = "Micheal Jordan"
+let myFloat = 23.23
+
+// ======== Lists ============
+
+// Square brackets create a list with semicolon delimiters.
+let twoToFive = [2;3;4;5]
+
+// :: creates list with new 1st element
+let oneToFive = 1 :: twoToFive
+
+// @ concats lists
+let zeroToFive = [0;1] @ twoToFive
