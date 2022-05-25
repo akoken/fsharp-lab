686284314952129fda6f69d9935201864726ce0a
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2022-05-25 19:40:45 +0300

    Add additional function examples

--- src/FSharpLab/functions.fs
+++ src/FSharpLab/functions.fs
@@ -1,13 +1,15 @@
 module FSharpLab.Functions
 
+// Define a function that takes an integer and returns the square of that integer
 let square x = x * x
 square 8
 
+// Define a function that takes two integers and returns the sum of those integers
 let add a b = a + b
 add 3 5
 
 
-// to define a multiline function, just use indents. No semicolons needed.
+// To define a multiline function, just use indents. No semicolons needed.
 // Define "isEven" as an inner ("nested") function
 // List.filter is a library function
 // with two parameters: a boolean function
@@ -19,3 +21,12 @@ let evens list =
 
 let oneToFive = [1;2;3;4;5]
 evens oneToFive
+
+// You can pipe the output of one operation to the next using "|>"
+// Here is the same sumOfSquares function written using pipes
+let sumOfSquaresTo100piped =
+    [1..100] |> List.map square |> List.sum // "square" was defined earlier in this file
+
+// You can define lambdas (anonymous functions) using the "fun" keyword
+let sumOfSquaresTo100withFun =
+    [1..100] |> List.map (fun x-> x*x) |> List.sum
